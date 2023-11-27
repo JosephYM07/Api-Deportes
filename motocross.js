a93de97db1ca2a645978e828afaa5056e365d30e
AzureAD\10800 <os.miguelparedes@gmail.com>
2023-11-26 21:19:36 -0500

    api motocross

--- motocross.js
+++ motocross.js
@@ -1,48 +1,113 @@
-document.getElementById('participantes').addEventListener('click', function() {
-    const url = 'https://www.thesportsdb.com/api/v1/json/3/eventresults.php?id=652890';
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const eventos = data.results;
-        const contenedor = document.getElementById('escritura');
-        const tabla = document.createElement('table');
-        let contadorColumnas = 0;
-        let filaActual;
+function listaJugadores() {
+  fetch('https://www.thesportsdb.com/api/v1/json/3/eventresults.php?id=652890')
+    .then(response => response.json())
+    .then(data => {
+      const eventos = data.results;
+      const contenedor = document.getElementById('escritura');
+      const tabla = document.createElement('table');
+
+      // Crear fila de encabezado
+      const encabezado = document.createElement('tr');
+
+      // Crear celda de "Participante"
+      const celdaParticipante = document.createElement('th');
+      celdaParticipante.textContent = "Participante";
+      encabezado.appendChild(celdaParticipante);
+
+      // Crear celda de "Puntaje"
+      const celdaPuntaje = document.createElement('th');
+      celdaPuntaje.textContent = "Puntaje";
+      encabezado.appendChild(celdaPuntaje);
+
+      // Crear celda de "Posición"
+      const celdaPosicion = document.createElement('th');
+      celdaPosicion.textContent = "Posición";
+      encabezado.appendChild(celdaPosicion);
+
+      // Agregar fila de encabezado a la tabla
+      tabla.appendChild(encabezado);
+
+      // Recorrer los eventos y agregar filas de datos a la tabla
+      for (let i = 0; i < eventos.length; i++) {
+        const fila = document.createElement('tr');
+
+        const celdaNombre = document.createElement('td');
+        celdaNombre.textContent = eventos[i].strPlayer;
+        fila.appendChild(celdaNombre);
+
+        const celdaPuntaje = document.createElement('td');
+        celdaPuntaje.textContent = eventos[i].intPoints;
+        fila.appendChild(celdaPuntaje);
+
+        const celdaPosicion = document.createElement('td');
+        celdaPosicion.textContent = eventos[i].intPosition;
+        fila.appendChild(celdaPosicion);
+
+        tabla.appendChild(fila);
+      }
+
+      tabla.style.width = '40%';
+      tabla.style.margin = '0 auto';
+      tabla.style.borderCollapse = 'collapse';
+
+      contenedor.innerHTML = ''; // Limpiar el contenido actual
+      contenedor.appendChild(tabla);
+    })
+    .catch(e => console.log(e));
+}
+
+
+document.getElementById('participantes').addEventListener('click', listaJugadores);
+document.getElementById('flecha-participantes').addEventListener('click', listaJugadores);
   
-        for (let i = 0; i < eventos.length; i++) {
-          if (contadorColumnas === 0) {
-            filaActual = document.createElement('tr');
-          }
-  
-          const celda = document.createElement('td');
-          celda.textContent = eventos[i].strPlayer;
-          filaActual.appendChild(celda);
-  
-          contadorColumnas++;
-  
-          if (contadorColumnas === 3 || i === eventos.length - 1) {
-            tabla.appendChild(filaActual);
-            contadorColumnas = 0;
-          }
-        }
-  
-        tabla.style.width = '60%';
-        tabla.style.margin = '0 auto';
-  
-        contenedor.innerHTML = ''; // Limpiar el contenido actual
-        contenedor.appendChild(tabla);
-      })
-      .catch(e => console.log(e));
-  });
-  
-  document.getElementById('otra-cosa').addEventListener('click', function() {
-    const prueba = 'Prueba';
+function paisesParticipantes (){
+  fetch('https://www.thesportsdb.com/api/v1/json/3/eventresults.php?id=652890')
+  .then(response => response.json())
+  .then(data => {
+    const eventos = data.results;
     const contenedor = document.getElementById('escritura');
-  
-    // Ocultar la información de la primera función
-    contenedor.innerHTML = '';
-  
-    // Mostrar la información de la segunda función
-    contenedor.style.textAlign = 'center';
-    contenedor.textContent = prueba;
-  });
+    const tabla = document.createElement('table');
+
+    // Crear fila de encabezado
+    const encabezado = document.createElement('tr');
+
+    // Crear celda de "Participante"
+    const celdaParticipante = document.createElement('th');
+    celdaParticipante.textContent = "Participante";
+    encabezado.appendChild(celdaParticipante);
+
+    // Crear celda de "País"
+    const celdaPais = document.createElement('th');
+    celdaPais.textContent = "País";
+    encabezado.appendChild(celdaPais);
+
+    // Agregar fila de encabezado a la tabla
+    tabla.appendChild(encabezado);
+
+    // Recorrer los eventos y agregar filas de datos a la tabla
+    for (let i = 0; i < eventos.length; i++) {
+      const fila = document.createElement('tr');
+
+      const celdaNombre = document.createElement('td');
+      celdaNombre.textContent = eventos[i].strPlayer;
+      fila.appendChild(celdaNombre);
+
+      const celdaPais = document.createElement('td');
+      celdaPais.textContent = eventos[i].strCountry;
+      fila.appendChild(celdaPais);
+
+      tabla.appendChild(fila);
+    }
+
+    tabla.style.width = '40%';
+    tabla.style.margin = '0 auto';
+    tabla.style.borderCollapse = 'collapse';
+
+    contenedor.innerHTML = ''; // Limpiar el contenido actual
+    contenedor.appendChild(tabla);
+  })
+  .catch(e => console.log(e));
+}
+
+  document.getElementById('paises').addEventListener('click', paisesParticipantes)
+  document.getElementById('flecha-paises').addEventListener('click', paisesParticipantes)
